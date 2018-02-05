75425d68634313c792798649e873b49d27a4e30f
lgx <gxliu@ikuai8.com>
2018-02-05 18:42:36 +0800

    end

--- list.py
+++ list.py
@@ -1,2 +1,15 @@
 classmates = ['tom','jack','peter']
 print(classmates[-1])
+
+#add in the end
+classmates.append('adam')
+print(classmates)
+
+#delete the end
+classmates.pop()
+print(classmates)
+
+tpl = ('1','2', ['3','4'])
+print(tpl)
+tpl[2][0] = '9'
+print(tpl)
